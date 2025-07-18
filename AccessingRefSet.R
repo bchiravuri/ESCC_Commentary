3a2a7c40cb4bc82e86e742d827916d3cf73827df
bchiravuri <bodhichiravuri@gmail.com>
2025-07-18 16:42:21 -0400

    small changes

--- AccessingRefSet.R
+++ AccessingRefSet.R
@@ -8,19 +8,18 @@ gr_genes <- readRDS("C:/Users/bodhi/AppData/Local/R/win-library/4.5/ces.refset.h
 
 gene_lengths <- as.data.table(gr_genes)[, .(length = sum(width(reduce(GRanges(seqnames, IRanges(start, end)))))), by = gene]
 
-reduce(GRanges(seqnames, IRanges(start, end)))
+write.csv(gene_lengths, "gene_lengths.csv")
 
-as.data.frame(gr_genes) %>%
-  group_by(gene) %>%
-  summarize(gene_length = sum(width(reduce)))
-
-
-
-genome_seqinfo <- seqinfo(BSgenome.Hsapiens.UCSC.hg38)
-seqlevels(gr_genes) <- paste0("chr", seqlevels(gr_genes))
-seqlevels(gr_genes) <- intersect(seqlevels(gr_genes), seqlevels(genome_seqinfo))
-seqinfo(gr_genes) <- genome_seqinfo[seqlevels(gr_genes)]
-
-seqlevels(gr_genes) <- str_replace(seqlevels(gr_genes), pattern = "chrchr", replacement = "chr")
-
-reduce(gr_genes)
+# as.data.frame(gr_genes) %>%
+#   group_by(gene) %>%
+#   summarize(gene_length = sum(width(reduce)))
+# 
+# 
+# genome_seqinfo <- seqinfo(BSgenome.Hsapiens.UCSC.hg38)
+# seqlevels(gr_genes) <- paste0("chr", seqlevels(gr_genes))
+# seqlevels(gr_genes) <- intersect(seqlevels(gr_genes), seqlevels(genome_seqinfo))
+# seqinfo(gr_genes) <- genome_seqinfo[seqlevels(gr_genes)]
+# 
+# seqlevels(gr_genes) <- str_replace(seqlevels(gr_genes), pattern = "chrchr", replacement = "chr")
+# 
+# reduce(gr_genes)
